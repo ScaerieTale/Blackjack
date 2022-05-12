aa01c77c0e72197a8dfdabf84cf84811a0affd98
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-11 21:15:38 -0500

    Deck is now properly generated at window load AND properly shuffled!

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // global variables needed for the game to function
-let dealerCardSum = 0;
-let playerCardSum = 0;
+var dealerCardSum = 0;
+var playerCardSum = 0;
 /* Because Aces can be 1 or 11 in Blackjack, it's important
 that we keep track of how many both the dealer and player
 are holding at any given time within a round. */
-let dealerHeldAces = 0;
-let playerHeldAces = 0;
+var dealerHeldAces = 0;
+var playerHeldAces = 0;
 
 // For the Dealer's hidden card
-let hidden;
+var hidden;
 
-let deck; // for the deck(s)
+var newDeck; // for the deck(s)
 
 /* playerCanHit means that you're not at 21 or above, and
 can draw another card. Default is true */
-let playerCanHit = true;
+var playerCanHit = true;
 
 // end of global variables
 
@@ -22,13 +22,38 @@ let playerCanHit = true;
 
 /* A set of functions to build the game deck as soon as the
 window finishes loading in the script */
-
-function buildCardDeck() {
-    let cardFaces = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-}
+/* cardFaces are the values, e.g. ace, king, 2, 3 etc.
+cardTypes are the suit e.g. hearts, spades.
+The buildDeck function concatonates the deck array from
+the two to create ace_of_clubs through king_of_spades,
+corresponding to the image names in /cards/ subfolder */
 
 window.onload = function() {
-    buildDeck();
+    buildCardDeck();
+    shuffle();
 }
 
+
+function buildCardDeck() {
+    let cardFaces = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+    let cardTypes = ["clubs", "diamonds", "hearts", "spades"];
+    newDeck = [];
+    for (let i = 0; i < cardTypes.length; i++) {
+        for (let j = 0; j < cardFaces.length; j++) {
+            newDeck.push(cardFaces[j] + "_of_" + cardTypes[i]);
+        }
+    }
+}
+/* In order to shuffle the deck and get a random card:
+Math.random times the length of thedeck array (e.g. 52),
+within Math.floor to round dpwn to the nearest integer */
+function shuffle() {
+    for (i = 0; i < newDeck.length; i++) {
+        let j = Math.floor(Math.random() * newDeck.length);
+        let temp = newDeck[i];
+        newDeck[i] = newDeck[j];
+        newDeck[j] = temp;
+    }
+    console.log(newDeck);
+}
 
